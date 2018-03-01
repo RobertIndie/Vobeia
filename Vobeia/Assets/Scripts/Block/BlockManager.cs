20ef5d119d040319142ff2a8deb2c851ff551a4c
AaronRobert <Robert.Aaron2017@outlook.com>
2018-03-01 21:04:25 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Vobeia/Assets/Scripts/Block/BlockManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager
+{
+    public static BlockManager instance = new BlockManager();
+    public Dictionary<byte, Type> blockDict = new Dictionary<byte, Type>();
+    /// <summary>
+    /// 注册方块
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="block"></param>
+    public void Register<T>(byte id) where T:Block
+    {
+        blockDict.Add(id, typeof(T));
+    }
+
+    public static void RegisterBlock()
+    {
+        instance.Register<GrassBlock>(1);
+    }
+}
