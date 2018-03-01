c22b67239975cf0a5cc847e9a0ed98139c8c2848
IIzzaya <linxuan_0709@icloud.com>
2018-03-01 19:41:55 +0800

    添加了Chunk的生成脚本

---
+++ Vobeia/Assets/Scripts/ChunksManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunksManager : MonoBehaviour {
+    public static ChunksManager instance;
+
+    public int chunkWidth = 20;
+    public int chunkHeight = 20;
+    public int seed = 0;
+    public float viewRange = 30;
+
+    public Chunk chunkFab;
+
+    void Awake() {
+        Cursor.visible = false;
+        instance = this;
+        if (seed == 0)
+            seed = Random.Range(0, int.MaxValue);
+    }
+
+    void Update() {
+        for (float x = transform.position.x - viewRange; x < transform.position.x + viewRange; x += chunkWidth)
+        {
+            for (float z = transform.position.z - viewRange; z < transform.position.z + viewRange; z += chunkWidth)
+            {
+
+                Vector3 pos = new Vector3(x, 0, z);
+                pos.x = Mathf.Floor(pos.x / (float)chunkWidth) * chunkWidth;
+                pos.z = Mathf.Floor(pos.z / (float)chunkWidth) * chunkWidth;
+
+                Chunk chunk = Chunk.FindChunk(pos);
+                if (chunk != null) continue;
+
+                chunk = (Chunk)Instantiate(chunkFab, pos, Quaternion.identity);
+
+            }
+        }
+    }
+}
